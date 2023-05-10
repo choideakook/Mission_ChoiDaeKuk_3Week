e9a8ca0c3062b8738d64f76950f51268a60ff840
choideakook <shdrnrhd113@naver.com>
2023-05-10 13:04:27 +0900

    feat: query dsl 세팅

---
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/repository/InstaMemberSearchCond.java
@@ -0,0 +1,11 @@
+package com.ll.gramgram.boundedContext.instaMember.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class InstaMemberSearchCond {
+}
--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -14,4 +14,5 @@ public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, 
     LikeablePerson findByFromInstaMemberIdAndToInstaMember_username(long fromInstaMemberId, String username);
 
     Optional<LikeablePerson> findByFromInstaMember_usernameAndToInstaMember_username(String fromInstaMemberUsername, String toInstaMemberUsername);
+
 }
